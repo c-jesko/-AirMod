743210528fd4cfba89ec78169855bb84cd5008d2
c-jesko <158676371+c-jesko@users.noreply.github.com>
2025-07-21 19:07:00 +0800

    Update ModModelsProvider.java

--- main/java/com/senna/air/datagen/ModModelsProvider.java
+++ main/java/com/senna/air/datagen/ModModelsProvider.java
@@ -15,30 +15,27 @@ public class ModModelsProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.HIGH_STEEL_INGOT_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MILE_STEEL_INGOT_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.HIGH_CARBON_STEEL_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MILE_CARBIN_STEEL_BLOCK);
 
 
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-        itemModelGenerator.register(ModItems.TINDER_WIRE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.FALMED_STRING, Models.GENERATED);
         itemModelGenerator.register(ModItems.WITHERED_BONE, Models.GENERATED);
         itemModelGenerator.register(ModItems.WITHERED_BONE_POWDER, Models.GENERATED);
-        itemModelGenerator.register(ModItems.HIGH_STEEL_INGOT, Models.GENERATED);
-        itemModelGenerator.register(ModItems.MILE_STEEL_INGOT, Models.GENERATED);
+        itemModelGenerator.register(ModItems.HIGH_CARBON_STEEL_INGOT, Models.GENERATED);
+        itemModelGenerator.register(ModItems.MILE_CARBON_STEEL_INGOT, Models.GENERATED);
 
-        itemModelGenerator.register(ModItems.HIGH_STEEL_INGOT_AXE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.HIGH_STEEL_INGOT_SHOVE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.HIGH_STEEL_INGOT_HOE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.HIGH_STEEL_INGOT_PICKAXE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.HIGH_STEEL_INGOT_SWORD, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.MILE_STEEL_INGOT_HOE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.MILE_STEEL_INGOT_AXE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.MILE_STEEL_INGOT_PICKAXE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.MILE_STEEL_INGOT_SHOVE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.MILE_STEEL_INGOT_SWORD, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.HIGH_CARBON_STEEL_AXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.HIGH_CARBON_STEEL_SHOVE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.HIGH_CARBON_STEEL_HOE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.HIGH_CARBON_STEEL_PICKAXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.HIGH_CARBON_STEEL_SWORD, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.MILE_CARBON_STEEL_SHOVE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.MILE_CARBON_STEEL_SWORD, Models.HANDHELD);
 
         itemModelGenerator.register(ModItems.FIRE_BOW, Models.HANDHELD);
 
