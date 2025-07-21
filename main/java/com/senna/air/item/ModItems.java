b59cbf806d272ed2449da8ae8ea1e2283cfdba31
c-jesko <158676371+c-jesko@users.noreply.github.com>
2025-07-21 19:49:33 +0800

    Update ModItems.java

--- main/java/com/senna/air/item/ModItems.java
+++ main/java/com/senna/air/item/ModItems.java
@@ -12,55 +12,55 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item TINDER_WIRE = registerItems("tinder_wire",new Item(new FabricItemSettings()));
+    public static final Item FLAMED_STRING = registerItems("flamed_string",new Item(new FabricItemSettings()));
     public static final Item WITHERED_BONE = registerItems("withered_bone",new Item(new FabricItemSettings()));
     public static final Item WITHERED_BONE_POWDER = registerItems("withered_bone_powder",new Item(new FabricItemSettings()));
-    public static final Item MILE_STEEL_INGOT = registerItems("mile_steel_ingot",new Item(new FabricItemSettings()));
-    public static final Item HIGH_STEEL_INGOT = registerItems("high_steel_ingot",new Item(new FabricItemSettings()));
+    public static final Item MILE_CARBON_STEEL_INGOT = registerItems("mile_carbon_steel_ingot",new Item(new FabricItemSettings()));
+    public static final Item HIGH_CRABON_STEEL_INGOT = registerItems("high_carbon_steel_ingot",new Item(new FabricItemSettings()));
 
 
 
-    public static final Item HIGH_STEEL_INGOT_PICKAXE = registerItems("high_steel_ingot_pickaxe",new PickaxeItem(ModToolMaterial.HIGH_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item HIGH_STEEL_INGOT_AXE = registerItems("high_steel_ingot_axe",new AxeItem(ModToolMaterial.HIGH_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item HIGH_STEEL_INGOT_SHOVE = registerItems("high_steel_ingot_shove",new ShovelItem(ModToolMaterial.HIGH_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item HIGH_STEEL_INGOT_SWORD = registerItems("high_steel_ingot_sword",new SwordItem(ModToolMaterial.HIGH_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item HIGH_STEEL_INGOT_HOE = registerItems("high_steel_ingot_hoe",new HoeItem(ModToolMaterial.HIGH_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item MILE_STEEL_INGOT_PICKAXE = registerItems("mile_steel_ingot_pickaxe",new PickaxeItem(ModToolMaterial.MILE_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item MILE_STEEL_INGOT_AXE = registerItems("mile_steel_ingot_axe",new AxeItem(ModToolMaterial.MILE_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item MILE_STEEL_INGOT_SHOVE = registerItems("mile_steel_ingot_shove",new ShovelItem(ModToolMaterial.MILE_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item MILE_STEEL_INGOT_SWORD = registerItems("mile_steel_ingot_sword",new SwordItem(ModToolMaterial.MILE_STEEL_INGOT,2,2f,new FabricItemSettings()));
-    public static final Item MILE_STEEL_INGOT_HOE = registerItems("mile_steel_ingot_hoe",new HoeItem(ModToolMaterial.MILE_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item HIGH_CARBON_STEEL_PICKAXE = registerItems("high_carbon_steel_pickaxe",new PickaxeItem(ModToolMaterial.HIGH_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item HIGH_CARBON_STEEL_AXE = registerItems("high_carbon_steel_axe",new AxeItem(ModToolMaterial.HIGH_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item HIGH_CARBON_STEEL_SHOVE = registerItems("high_carbon_steel_shove",new ShovelItem(ModToolMaterial.HIGH_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item HIGH_CARBON_STEEL_SWORD = registerItems("high_carbon_steel_sword",new SwordItem(ModToolMaterial.HIGH_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item HIGH_CARBON_STEEL_HOE = registerItems("high_carbon_steel_hoe",new HoeItem(ModToolMaterial.HIGH_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item MILE_CARBON_STEEL_PICKAXE = registerItems("mile_carbon_steel_pickaxe",new PickaxeItem(ModToolMaterial.MILE_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item MILE_CARBON_STEEL_AXE = registerItems("mile_carbon_steel_axe",new AxeItem(ModToolMaterial.MILE_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item MILE_CARBON_STEEL_SHOVE = registerItems("mile_carbon_steel_shove",new ShovelItem(ModToolMaterial.MILE_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item MILE_CARBON_STEEL_SWORD = registerItems("mile_carbon_steel_sword",new SwordItem(ModToolMaterial.MILE_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
+    public static final Item MILE_CARBON_STEEL_HOE = registerItems("mile_carbon_steel_hoe",new HoeItem(ModToolMaterial.MILE_CARBON_STEEL_INGOT,2,2f,new FabricItemSettings()));
 
     public static final Item FIRE_BOW = registerItems("fire_bow", new EnchantedBowItem(new FabricItemSettings().maxDamage(500),Enchantments.FLAME));
 
 
     private static void addItemsToIG(FabricItemGroupEntries fabricItemGroupEntries){   //加入原材料的
-        fabricItemGroupEntries.add(TINDER_WIRE);
+        fabricItemGroupEntries.add(FLAMED_STRING);
         fabricItemGroupEntries.add(WITHERED_BONE);
         fabricItemGroupEntries.add(WITHERED_BONE_POWDER);
-        fabricItemGroupEntries.add(MILE_STEEL_INGOT);
-        fabricItemGroupEntries.add(HIGH_STEEL_INGOT);
+        fabricItemGroupEntries.add(MILE_CARBON_STEEL_INGOT);
+        fabricItemGroupEntries.add(HIGH_CARBON_STEEL_INGOT);
 
 
     }
 
     private static void addItemsToIG1(FabricItemGroupEntries fabricItemGroupEntries) {  //加入工具的
 
-        fabricItemGroupEntries.add(MILE_STEEL_INGOT_SHOVE);
-        fabricItemGroupEntries.add(MILE_STEEL_INGOT_PICKAXE);
-        fabricItemGroupEntries.add(MILE_STEEL_INGOT_AXE);
-        fabricItemGroupEntries.add(MILE_STEEL_INGOT_HOE);
-        fabricItemGroupEntries.add(HIGH_STEEL_INGOT_SHOVE);
-        fabricItemGroupEntries.add(HIGH_STEEL_INGOT_PICKAXE);
-        fabricItemGroupEntries.add(HIGH_STEEL_INGOT_AXE);
-        fabricItemGroupEntries.add(HIGH_STEEL_INGOT_HOE);
+        fabricItemGroupEntries.add(MILE_CARBON_STEEL_SHOVE);
+        fabricItemGroupEntries.add(MILE_CARBON_STEEL_PICKAXE);
+        fabricItemGroupEntries.add(MILE_CARBON_STEEL_AXE);
+        fabricItemGroupEntries.add(MILE_CARBON_STEEL_HOE);
+        fabricItemGroupEntries.add(HIGH_CARBON_STEEL_SHOVE);
+        fabricItemGroupEntries.add(HIGH_CARBON_STEEL_PICKAXE);
+        fabricItemGroupEntries.add(HIGH_CARBON_STEEL_AXE);
+        fabricItemGroupEntries.add(HIGH_CARBON_STEEL_HOE);
 
     }
     private static void addItemsToIG2(FabricItemGroupEntries fabricItemGroupEntries){//加入武器装备的
-        fabricItemGroupEntries.add(MILE_STEEL_INGOT_SWORD);
-        fabricItemGroupEntries.add(HIGH_STEEL_INGOT_SWORD);
-        fabricItemGroupEntries.add(MILE_STEEL_INGOT_AXE);
-        fabricItemGroupEntries.add(HIGH_STEEL_INGOT_AXE);
+        fabricItemGroupEntries.add(MILE_CARBON_STEEL_SWORD);
+        fabricItemGroupEntries.add(HIGH_CARBON_STEEL_SWORD);
+        fabricItemGroupEntries.add(MILE_CARBON_STEEL_AXE);
+        fabricItemGroupEntries.add(HIGH_CARBON_STEEL_AXE);
         fabricItemGroupEntries.add(FIRE_BOW);
 
     }
